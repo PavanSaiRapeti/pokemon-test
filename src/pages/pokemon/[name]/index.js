fb0bb0c2d7463da1522fabd56ac266ca609f0a57
Pavan Rapeti <Pavansairapeti28@gmail.com>
2025-03-29 15:58:01 +0000

    push

--- src/pages/pokemon/[name]/index.js
+++ src/pages/pokemon/[name]/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { fetchPokemonDetails } from '../../../services/ApiService'
+import { fetchPokemonDetails } from '../../../services/apiService'
 import { useRouter } from 'next/router'
 
 const LandingPage = ({ pokemon }) => {
@@ -15,11 +15,25 @@ const LandingPage = ({ pokemon }) => {
           <div className='flex flex-col items-center border-2  bg-white rounded-lg w-1/4 '>
             <img src={pokemon.sprites.front_default} alt={pokemon.name} className='mb-4  w-[400px]' />
             <div className='bg-[#fdc666] rounded-lg p-6 '>
-              <h1 className='text-2xl mb-2'><b>Name:</b> {pokemon.name}</h1>
-              <p className='mb-2'><b>Type:</b> {pokemon.types.map((type) => type.type.name).join(', ')}</p>
-              <p className='mb-2'><b>Stats:</b> {pokemon.stats.map((stat) => stat.stat.name).join(', ')}</p>
-              <p className='mb-2'><b>Abilities:</b> {pokemon.abilities.map((ability) => ability.ability.name).join(', ')}</p>
-              <p><b>Some Moves:</b> {pokemon.moves.slice(0, 5).map((move) => move.move.name).join(', ')}</p>  
+              <h1 className='text-2xl mb-2'>
+                <b>Name:</b> {pokemon.name}
+              </h1>
+              <p className='mb-2'>
+                <b>Type:</b> {pokemon.types.map((type) => type.type.name).join(', ')}
+              </p>
+              <p className='mb-2'>
+                <b>Stats:</b> {pokemon.stats.map((stat) => stat.stat.name).join(', ')}
+              </p>
+              <p className='mb-2'>
+                <b>Abilities:</b> {pokemon.abilities.map((ability) => ability.ability.name).join(', ')}
+              </p>
+              <p>
+                <b>Some Moves:</b>{' '}
+                {pokemon.moves
+                  .slice(0, 5)
+                  .map((move) => move.move.name)
+                  .join(', ')}
+              </p>
             </div>
           </div>
         ) : (
